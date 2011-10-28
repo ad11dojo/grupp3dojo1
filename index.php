36f3149b76b6d44091ae4b33a974599d0bb96c5e
AD11 Koddojo <dojo@kgm.cc>
2011-10-28 16:14:38 +0200

    fixat array och funktion

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <html>
-<head><title>AD11 grupp3dojo1</title></head>
+<head><meta charset="UTF-8"><title>AD11 grupp3dojo1</title></head>
 <body>
 <?php
 
-echo "hello world!";
+// detta är vår kända input & vad vi vill få ut:
+$test_input = "**.|…|…";
+$test_output = "bb1|221|000";
+$input = "xxx|x*x|xxx";
 
+// skriv en funktion som tar in en input-sträng & returnerar en output-sträng
+$output = ms( $input );
 
+function ms( $stjärna )//Spela ms
+{
+	$a = explode ( "|" , $stjärna );
+	print_r ($a);
+	echo "<br />";
+
+	$ms = array( str_split($a[0]), str_split($a[1]), str_split($a[2]));
+
+	print_r ($ms);
+
+	echo "<br />";
+	
+	return "$stjärna";
+}
+
+// testa om det funkar?
+if( $test_output == ms( $test_input ) )
+{
+	echo "bra!";
+}
+else
+{
+	echo "öva mer!";
+}
 
 
 ?>
